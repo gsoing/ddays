68fb0d70c220af3e66af2bbfc579e1194c17cc82
Cédric Bazureau <cedric.bazureau@renault.com>
2017-10-10 17:09:39 +0200

    Questions

--- front/src/ChatBot/ChatBot.js
+++ front/src/ChatBot/ChatBot.js
@@ -3,6 +3,26 @@ import React, { Component } from 'react';
 import Bubble from './Bubble/Bubble';
 import './ChatBot.css';
 
+const getRandomInt = (min, max) => Math.floor(Math.random() * ((max - min) + 1)) + min;
+
+const QUESTIONS = [
+  {
+    question: "Yo youy you",
+  },
+  {
+    question: "Yo youy you 2",
+  },
+  {
+    question: "Yo youy you 3",
+  },
+  {
+    question: "Yo youy you 4",
+  },
+  {
+    question: "Yo youy you 5",
+  }
+];
+
 class ChatBot extends Component {
 
     /**
@@ -12,13 +32,10 @@ class ChatBot extends Component {
       super(props);
 
       this.state = {
+        questions: QUESTIONS,
         items: [
           {
             entry: "Hello ! Welcome to the car chooser",
-            isBot: true,
-          },
-          {
-            entry: "How many people live in your house ?",
             isBot: true,
           }
         ]
@@ -29,20 +46,36 @@ class ChatBot extends Component {
       if(event.key == 'Enter' && event.target.value !== ''){
         console.log('enter press here! ', event.target.value)
         this.setState({
-          items: [...this.state.items, {
-            entry: event.target.value,
-            isBot: false,
-          }],
+          items: [
+            ...this.state.items,
+            {
+              entry: event.target.value,
+              isBot: false,
+            },
+            this.getRandomQuestion()
+          ],
         });
         if (this.props.handleNewAnswer) {
           this.props.handleNewAnswer(event.target.value);
         }
         event.target.value = '';
+        this.getRandomQuestion();
       }
     }
 
     getRandomQuestion = () => {
+      const index = getRandomInt(0,this.state.questions.length-1);
+      return {
+        entry: this.state.questions[index].question,
+        isBot: true,
+      };
 
+    }
+
+    componentDidMount() {
+      this.setState({
+        items: [...this.state.items, this.getRandomQuestion()]
+      });
     }
 
     render() {
