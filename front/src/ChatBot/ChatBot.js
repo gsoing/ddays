a89d794d423b25c1060dcac3faa34bf2e9cbcb6b
Cédric Bazureau <cedric.bazureau@renault.com>
2017-10-10 16:06:11 +0200

    Chatbot

--- front/src/ChatBot/ChatBot.js
+++ front/src/ChatBot/ChatBot.js
@@ -4,22 +4,53 @@ import Bubble from './Bubble/Bubble';
 import './ChatBot.css';
 
 class ChatBot extends Component {
+
+    /**
+     * @private
+     */
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        items: [
+          {
+            entry: "Hello ! Welcome to the car chooser",
+            isBot: true,
+          },
+          {
+            entry: "How many people live in your house ?",
+            isBot: true,
+          }
+        ]
+      };
+    }
+
+    handleKeyPress = (event) => {
+      if(event.key == 'Enter'){
+        console.log('enter press here! ', event.target.value)
+        /*this.setState({
+          criteria,
+        });*/
+      }
+    }
+
+    componentWillMount() {
+      // TODO
+    }
+
+
     render() {
+      const { items } = this.state;
       return (
         <div className="ChatBot">
           <div className="ChatBot__chat">
-            <Bubble isBot>
-              Hello ! Welcome to the car chooser
-            </Bubble>
-            <Bubble isBot>
-              How many people live in your house
-            </Bubble>
+            { items.map((item, index) => <Bubble key={`it-${index}`} isBot={item.isBot}>{item.entry}</Bubble>) }
             <Bubble>
               We are 5, 2 adults and 3 children
             </Bubble>
           </div>
           <div className="ChatBot__form">
-            <input className="ChatBot__input" placeholder="..." />
+            <input className="ChatBot__input" id="chat" placeholder="..." onKeyPress={this.handleKeyPress} />
           </div>
         </div>)
     }
