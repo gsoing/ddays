2f3aa11c98bbb8764ce68751747cad3ed7feac9e
ddarcy <dilhan14@hotmail.fr>
2017-10-10 15:32:50 +0200

    setup vehicle list

--- front/src/ResultPage/ResultPage.js
+++ front/src/ResultPage/ResultPage.js
@@ -1,11 +1,92 @@
 import React from 'react';
+import VehicleItem from '../VehicleItem/VehicleItem';
 
-// import './ResultPage.css';
+ import './ResultPage.css';
 
-const ResultPage = ({ title }) => (
-  <div className="ResultPage">
-    ResultPage
-  </div>
-);
+const ResultPage = ({ title }) => {
+  const vehicles = [{
+      brand: 'Renault', 
+      modelLabel: 'Clio',
+      versionLabel: 'version',
+      image: [{
+        type: '',
+        url: 'http://lorempixel.com/1024/768/',
+      }],
+      price: '24790.0',
+      year: 'year',
+      fuel: 'fuel',
+      mileage: 'mileage',
+    },
+    {
+      brand: 'Renault', 
+      modelLabel: 'Clio',
+      versionLabel: 'version',
+      image: [{
+        type: '',
+        url: 'http://lorempixel.com/1024/768/',
+      }],
+      price: '24790.0',
+      year: 'year',
+      fuel: 'fuel',
+      mileage: 'mileage',
+    },
+    {
+      brand: 'Renault', 
+      modelLabel: 'Clio',
+      versionLabel: 'version',
+      image: [{
+        type: '',
+        url: 'http://lorempixel.com/1024/768/',
+      }],
+      price: '24790.0',
+      year: 'year',
+      fuel: 'fuel',
+      mileage: 'mileage',
+    },
+    {
+      brand: 'Renault', 
+      modelLabel: 'Clio',
+      versionLabel: 'version',
+      image: [{
+        type: '',
+        url: 'http://lorempixel.com/1024/768/',
+      }],
+      price: '24790.0',
+      year: 'year',
+      fuel: 'fuel',
+      mileage: 'mileage',
+    },
+    {
+      brand: 'Renault', 
+      modelLabel: 'Clio',
+      versionLabel: 'version',
+      image: [{
+        type: '',
+        url: 'http://lorempixel.com/1024/768/',
+      }],
+      price: '24790.0',
+      year: 'year',
+      fuel: 'fuel',
+      mileage: 'mileage',
+    }
+  ];
+
+  return (
+    <div className="ResultPage">
+      <div className="ResultPage__list">
+      {
+        vehicles.map(vehicle => (
+          <div className="ResultPage__listItem">
+            <VehicleItem vehicle={vehicle} />
+          </div>
+        ))
+      }{
+        vehicles && vehicles.length % 3 === 2 &&
+        <div className="ResultPage__listItem"></div>
+      }
+      </div>
+    </div>
+  )
+};
 
 export default ResultPage;
--- front/src/VehicleItem/VehicleItem.js
+++ front/src/VehicleItem/VehicleItem.js
@@ -5,27 +5,34 @@ import './VehicleItem.css';
 const VehicleItem = ({vehicle}) => {
 
     const { 
-        brand, //marque
-        model, //model
-        version, //version
-        finish //finition
-        picture, //img vehicule
-        year, // année
-        fuel, //carburant
-        mileage, //mileage
-    } : vehicle;
+        brand, 
+        modelLabel,
+        versionLabel,
+        image,
+        price,
+        year,
+        fuel,
+        mileage,
+    } = vehicle;
 
     return (
         <div className="VehicleItem">
-            <img src={picture} alt="default" className="VehicleItem__picture"/>
-            <p className="VehicleItem__title">{brand} {model}</p>
-            <p className="VehicleItem__subtitle">{version} {finish}</p>
-            <ul className="VehicleItem__infos">
-                <li className="VehicleItem__info">{year}</li>
-                <li className="VehicleItem__info">{fuel}</li>
-                <li className="VehicleItem__info">{mileage}</li>
-            </ul>
-            <button className="VehicleItem__chooseButton">Choose</button>
+            <div className="VehicleItem__pictureBox">
+                <img src={image[0].url} alt="default" className="VehicleItem__picture"/>
+            </div>      
+            <div className="VehicleItem__content">
+                <p className="VehicleItem__title">{brand} {modelLabel}</p>
+                <p className="VehicleItem__subtitle">{versionLabel}</p>
+                <ul className="VehicleItem__infos">
+                    <li className="VehicleItem__info">{year}</li>
+                    <li className="VehicleItem__info">{fuel}</li>
+                    <li className="VehicleItem__info">{mileage}</li>
+                </ul>
+                <p className="VehicleItem__price">{Intl.NumberFormat("fr-FR").format(price)} €</p>
+            </div>
+            <button className="VehicleItem__chooseButton">Select</button>
         </div>
     )
 }
+
+export default VehicleItem;
