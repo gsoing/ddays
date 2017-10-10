eb497b0d55b10bebc5aa9ac19cfa6ab489ce407d
ddarcy <dilhan14@hotmail.fr>
2017-10-10 17:04:59 +0200

    add loading and remove mock

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import FunnelHeader from './FunnelHeader/FunnelHeader';
@@ -6,13 +7,54 @@ import ChatBot from './ChatBot/ChatBot';
 import ResultPage from './ResultPage/ResultPage';
 
 class App extends Component {
+
+  /**
+   * @private
+   */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentParams: [],
+      vehicles: [],
+      total: null,
+      loading: true,
+    };
+  }
+
+  refreshVehicleListe = (lastAnswer) => {
+    const payload =  {
+      lastAnswer,
+      currentParams: this.state.currentParams,
+    };
+    this.setState({loading: true});
+    axios.post('http://localhost:3003/api/test', payload)
+    .then((response) => {
+      this.setState({
+        ...response.data,
+        loading: false,
+      })
+    });
+  }
+
+  componentDidMount(){
+    this.refreshVehicleListe();
+  }
+
   render() {
+    const { vehicles, currentParams, loading } = this.state;
     return (
       <div className="App">
         <FunnelHeader title="DDays 2017" />
         <div className="Layout">
-          <div className="Layout__ChatBot"><ChatBot /></div>
-          <div className="Layout__ResultPage"><ResultPage /></div>
+          <div className="Layout__ChatBot"><ChatBot handleNewAnswer={(a) => this.refreshVehicleListe(a)} /></div>
+          <div className="Layout__ResultPage">
+            <ResultPage 
+              vehicles={vehicles} 
+              currentParams={currentParams}
+              loading={loading}
+            />
+          </div>
         </div>
       </div>
     );
--- front/src/ResultPage/ResultPage.js
+++ front/src/ResultPage/ResultPage.js
@@ -3,98 +3,24 @@ import VehicleItem from '../VehicleItem/VehicleItem';
 
  import './ResultPage.css';
 
-const ResultPage = ({ title }) => {
-  const vehicles = [{
-      brand: 'Renault', 
-      modelLabel: 'Clio',
-      versionLabel: 'version',
-      image: [{
-        type: '',
-        url: 'http://lorempixel.com/1024/768/',
-      }],
-      price: '24790.0',
-      year: '2015',
-      energy: 'diesel',
-      mileage: '1000',
-      pollution: '',
-      vehicleType:'VO',
-    },
-    {
-      brand: 'Renault', 
-      modelLabel: 'Clio',
-      versionLabel: 'version',
-      image: [{
-        type: '',
-        url: 'http://lorempixel.com/1024/768/',
-      }],
-      price: '24790.0',
-      year: '2015',
-      energy: 'diesel',
-      mileage: '1000',
-      pollution: '',
-      vehicleType:'VN',
-    },
-    {
-      brand: 'Renault', 
-      modelLabel: 'Clio',
-      versionLabel: 'version',
-      image: [{
-        type: '',
-        url: 'http://lorempixel.com/1024/768/',
-      }],
-      price: '24790.0',
-      year: '2015',
-      energy: 'diesel',
-      mileage: '1000',
-      pollution: '',
-      vehicleType:'VO',
-    },
-    {
-      brand: 'Renault', 
-      modelLabel: 'Clio',
-      versionLabel: 'version',
-      image: [{
-        type: '',
-        url: 'http://lorempixel.com/1024/768/',
-      }],
-      price: '24790.0',
-      year: '2015',
-      energy: 'diesel',
-      mileage: '1000',
-      pollution: '',
-      vehicleType:'VN',
-    },
-    {
-      brand: 'Renault', 
-      modelLabel: 'Clio',
-      versionLabel: 'version',
-      image: [{
-        type: '',
-        url: 'http://lorempixel.com/1024/768/',
-      }],
-      price: '24790.0',
-      year: '2015',
-      energy: 'diesel',
-      mileage: '1000',
-      pollution: '',
-      vehicleType:'VO',
-    }
-  ];
+const ResultPage = ({ vehicles, currentParams, loading }) => {
 
   return (
     <div className="ResultPage">
-      <div className="ResultPage__list">
-      {
-        vehicles.map(vehicle => (
-          <div className="ResultPage__listItem">
-            <VehicleItem vehicle={vehicle} />
-          </div>
-        ))
-      }{
-        vehicles && vehicles.length % 3 === 2 &&
-        <div className="ResultPage__listItem"></div>
+      { !loading && 
+        <div className="ResultPage__list">
+        {
+          vehicles.map(vehicle => (
+            <div className="ResultPage__listItem">
+              <VehicleItem vehicle={vehicle} />
+            </div>
+          ))
+        }{
+          vehicles && vehicles.length % 3 === 2 &&
+          <div className="ResultPage__listItem"></div>
+        }
+        </div>
       }
-      </div>
     </div>
   )
 };
