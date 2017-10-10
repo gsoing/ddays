3bda65ca6a15ebbda11b957f7fe17c35cb687d42
ddarcy <dilhan14@hotmail.fr>
2017-10-10 16:07:17 +0200

    add vehicle type

--- front/src/VehicleItem/VehicleItem.js
+++ front/src/VehicleItem/VehicleItem.js
@@ -11,26 +11,29 @@ const VehicleItem = ({vehicle}) => {
         image,
         price,
         year,
-        fuel,
+        energy,
         mileage,
+        pollution,
+        vehicleType,
     } = vehicle;
 
     return (
         <div className="VehicleItem">
             <div className="VehicleItem__pictureBox">
                 <img src={image[0].url} alt="default" className="VehicleItem__picture"/>
+                <span className="VehicleItem__vehicleType">{vehicleType === 'VO' ? 'SECOND-HAND' :  'NEW'}</span>
             </div>      
             <div className="VehicleItem__content">
                 <p className="VehicleItem__title">{brand} {modelLabel}</p>
                 <p className="VehicleItem__subtitle">{versionLabel}</p>
                 <ul className="VehicleItem__infos">
                     <li className="VehicleItem__info">{year}</li>
-                    <li className="VehicleItem__info">{fuel}</li>
-                    <li className="VehicleItem__info">{mileage}</li>
+                    <li className="VehicleItem__info">{energy}</li>
+                    <li className="VehicleItem__info">{mileage} km</li>
                 </ul>
                 <p className="VehicleItem__price">{Intl.NumberFormat("fr-FR").format(price)} €</p>
             </div>
-            <button className="VehicleItem__chooseButton">Select</button>
+            <button className="VehicleItem__chooseButton">SELECT</button>
         </div>
     )
 }
