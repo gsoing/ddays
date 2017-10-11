fbb57c4d2d15ebc91421563d9d698c3bd5609937
ddarcy <dilhan14@hotmail.fr>
2017-10-11 14:39:28 +0200

    set CORS node app

--- node/server.js
+++ node/server.js
@@ -12,6 +12,18 @@ server.listen(3001,'localhost',function(){
 app.use(BodyParser.urlencoded({extended:true}));
 app.use(BodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Accept, Cache-Control, Origin, Content-Type");
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 var wishList = null;
 var suggest = null;
 
